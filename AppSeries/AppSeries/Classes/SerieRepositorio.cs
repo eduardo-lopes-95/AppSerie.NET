69e5dcb819b3b5fbe68873404a3a25c666f78527
Eduardo Sabino <eduardosabino@ymail.com>
2021-08-27 14:02:37 -0300

    app series .net poo

---
+++ AppSeries/AppSeries/Classes/SerieRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using AppSeries.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSeries.Classes
+{
+    class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>(); //bd in memory
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Excluir(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+
+    }
+}
